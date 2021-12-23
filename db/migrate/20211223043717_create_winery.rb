0133b94052944b2b21e83ab12121793a3e7b3b1e
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-12-22 23:42:20 -0500

    Migrated

--- db/migrate/20211223043717_create_winery.rb
+++ db/migrate/20211223043717_create_winery.rb
@@ -1,7 +1,10 @@
 class CreateWinery < ActiveRecord::Migration[5.2]
   def change
     create_table :wineries do |t|
-      
+      t.string :name, null: false
+      t.string :country, null: false
     end
+
+    add_index :wineries, [:name, :country], unique: true
   end
 end
