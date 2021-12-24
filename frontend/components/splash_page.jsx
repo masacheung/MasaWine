fea048d6dee46770073f3b5643de730179fe084e
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-12-23 22:50:36 -0500

    MasaWine

--- frontend/components/splash_page.jsx
+++ frontend/components/splash_page.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const SplashPages = () => {
     return (
     <div className="splash">
+        <header>
+            <div className="navbar">
+                <Link to="/" className="logo-navbar">
+                    <img src={window.wine} className="logo-nav"/>
+                    <p>MasaWine</p>
+                </Link>
+                <ul className="ul-nav">
+                    <li>
+                        <a href="https://github.com/masacheung" target="_blank" rel="noreferrer noopener">Github</a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/man-tat-masa-cheung-725b39b8/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
+                    </li>
+                    <li>
+                        <a href="https://angel.co/u/man-tat-masa-cheung" target="_blank" rel="noreferrer noopener">AngelList</a>
+                    </li>
+                    <li>
+                        <a href="https://masacheung.github.io/portfolio/" target="_blank" rel="noreferrer noopener">Portfolio</a>
+                    </li>
+                </ul>
+            </div>
+        </header>
         <div className="splash-slogan">
             <div className="slogan">
                 MasaWine~~~
