93b82f1d33890a09dec8230d4fc058c475bdf91e
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-12-23 23:40:32 -0500

    Splash Pge

--- frontend/components/splash_page.jsx
+++ frontend/components/splash_page.jsx
@@ -15,13 +15,7 @@ const SplashPages = () => {
                         <a href="https://github.com/masacheung" target="_blank" rel="noreferrer noopener">Github</a>
                     </li>
                     <li>
-                        <a href="https://www.linkedin.com/in/man-tat-masa-cheung-725b39b8/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
-                    </li>
-                    <li>
-                        <a href="https://angel.co/u/man-tat-masa-cheung" target="_blank" rel="noreferrer noopener">AngelList</a>
-                    </li>
-                    <li>
-                        <a href="https://masacheung.github.io/portfolio/" target="_blank" rel="noreferrer noopener">Portfolio</a>
+                        <Link to="/wineries" className="logo-navbar">Wineries</Link>
                     </li>
                 </ul>
             </div>
