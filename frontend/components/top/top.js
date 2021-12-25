f5b10d4e204e4b9d293b0203b7a3acb83a496d9e
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-12-25 16:54:19 -0500

    Error about axios call

--- frontend/components/top/top.js
+++ frontend/components/top/top.js
@@ -10,11 +10,11 @@ export default class Top extends React.Component {
     }
 
     componentDidMount(){
-        axios.get("https://top-100-example.s3.amazonaws.com/t100_2021_full.json")
-            .then(res => {
-                console.log(res.data);
-                this.setState({top: res.data})
-            })
+        // axios.get("https://top-100-example.s3.amazonaws.com/t100_2021_full.json")
+        //     .then(res => {
+        //         console.log(res.data);
+        //         this.setState({top: res.data})
+        //     })
     }
 
     render() {
