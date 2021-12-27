b27e015cc549b03db0295c1981e91cbeba53d647
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-12-27 16:00:48 -0500

    Splash Page Styling

--- frontend/components/splash_page.jsx
+++ frontend/components/splash_page.jsx
@@ -4,25 +4,6 @@ import Top from "./top/top"
 const SplashPages = () => {
     return (
     <div className="splash">
-        {/* <header>
-            <div className="navbar">
-                <Link to="/" className="logo-navbar">
-                    <img src={window.wine} className="logo-nav"/>
-                    <p>MasaWine</p>
-                </Link>
-                <ul className="ul-nav">
-                    <li>
-                        <a href="https://github.com/masacheung" target="_blank" rel="noreferrer noopener">Github</a>
-                    </li>
-                    <li>
-                        <Link to="/wineries">Wineries</Link>
-                    </li>
-                    <li>
-                        <Link to="/wines">Wines</Link>
-                    </li>
-                </ul>
-            </div>
-        </header> */}
         <div className="splash-slogan">
             <div className="slogan">
                 TOP 100 LISTS
