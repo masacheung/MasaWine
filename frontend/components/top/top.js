f6179b53b39ebfecafddcb270e98dcbd237773c8
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-12-27 12:17:01 -0500

    Axios

--- frontend/components/top/top.js
+++ frontend/components/top/top.js
@@ -11,7 +11,7 @@ export default class Top extends React.Component {
     }
 
     componentDidMount(){
-        axios.get("https://cors-anywhere.herokuapp.com/https://top-100-example.s3.amazonaws.com/t100_2021_full.json")
+        axios.get(`https://cors-anywhere.herokuapp.com/https://top-100-example.s3.amazonaws.com/t100_2021_full.json`)
             .then(res => {
                 console.log(res.data);
                 this.setState({top: res.data})
