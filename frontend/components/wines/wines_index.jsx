3941147b20dbf1bc753296e083f75bc33c2055ab
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-12-27 21:45:26 -0500

    Wine Create

--- frontend/components/wines/wines_index.jsx
+++ frontend/components/wines/wines_index.jsx
@@ -3,10 +3,48 @@ import React from "react";
 export default class WinesIndex extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            wineFull: "",
+            vintage: "",
+            color: "",
+            country: "",
+            region: "",
+            alternateBottleSize: "",
+            wineryId: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount(){
         this.props.fetchWines();
+    }
+
+    update(field){
+        return e => {
+            this.setState({[field]: e.currentTarget.value})
+        }
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        let winery = {
+            wine_full: this.state.wine_full,
+            vintage: this.state.vintage,
+            color: this.state.color,
+            country: this.state.country,
+            region: this.state.region,
+            alternate_bottle_size: this.state.alternateBottleSize,
+            wineryId: this.state.wineryId
+        }
+
+        this.props.createWinery(winery);
+        this.setState({wineFull: "",
+                    vintage: "",
+                    color: "",
+                    country: "",
+                    region: "",
+                    alternateBottleSize: "",
+                    wineryId: ""})
     }
 
     render() {
