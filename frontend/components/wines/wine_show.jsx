29acfd728cb3d5e8da01d9803a95ae9ad8f202c7
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-12-31 00:22:00 -0500

    Wine Show Page

--- frontend/components/wines/wine_show.jsx
+++ frontend/components/wines/wine_show.jsx
@@ -53,22 +53,40 @@ export default class WineShow extends React.Component{
                 <div className="splash-slogan">
                     <div className="slogan">{this.props.wine.wine_full} Wine</div>
                 </div>
+                <div>
+                    <img src={window.bottle} className='wine-show-img'/>
+                    <div className="wine-info">
+                        <div className="info-detail"><div className="info-name">Wine:</div> <div className='details'>{this.props.wine.wine_full}</div></div>
+                        <div className="info-detail"><div className="info-name">Winery:</div> <div className='details'>{this.props.wine.winery_id}</div></div>
+                        <div className="info-detail"><div className="info-name">Country: </div><div className='details'>{this.props.wine.country}</div></div>
+                        <div className="info-detail"><div className="info-name">Region:</div> <div className='details'>{this.props.wine.region}</div></div>
+                        <div className="info-detail"><div className="info-name">Vintage:</div> <div className='details'>{this.props.wine.vintage}</div></div>
+                        <div className="info-detail"><div className="info-name">Color:</div> <div className='details'>{this.props.wine.color}</div></div>
+                        <div className="info-detail"><div className="info-name">Size: </div>
+                            {this.props.wine.alternate_bottle_size === "" ? <div className="details">N/A</div> : <div className="details">{this.props.wine.alternate_bottle_size} mL</div>}
+                        </div>
+                    </div>
+                </div>
                 <div className='tasting-notes-list'>
-                <ul className="tasting-notes-ul">
-                    {this.props.tasting_notes.map(tasting_note => 
-                        <li key={tasting_note.id}>
-                            <div className='sub-header'>
-                                <div className="username">
-                                    Username:&nbsp; <b>{tasting_note.username}</b>
+                    <ul className="tasting-notes-ul">
+                        {this.props.tasting_notes.map(tasting_note => 
+                            <li key={tasting_note.id}>
+                                <div className='sub-header'>
+                                    <div className="username">
+                                        Username:&nbsp; <b>{tasting_note.username}</b>
+                                    </div>
+                                    <div className='title'>Title: {tasting_note.title}</div>
+                                    <div className='delete'>
+                                    <button className="delete-button" onClick={() => this.handleDelete(tasting_note.id)}>Delete</button>
                                 </div>
-                                <div className='sub-header-comment'>Tasting Note: {tasting_note.body}</div>
-                            </div>
-                            <div className='sub-header-title'>Title: {tasting_note.title}</div>
-                            <div className='sub-header-delete'>
-                                <button className="delete-button" onClick={() => this.handleDelete(tasting_note.id)}>Delete</button>
-                            </div>
-                        </li>)}
-                </ul>
+                                </div>
+                                <div className="sub-header">
+                                    Tasting Note:
+                                    <div className='sub-header-comment'>{tasting_note.body}</div>
+                                </div>
+
+                            </li>)}
+                    </ul>
                 <form className="tasting-note-form" onSubmit={this.handleSubmit}>
                     <lable className="tasting-note-name">Username: </lable>
                     <input className="tasting-note-input" type="text" placeholder="Username" value={this.state.username} onChange={this.update("username")}/>
